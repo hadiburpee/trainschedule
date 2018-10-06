856c653880d35e26bcbb7ed97f88db108e158278
hadiburpee <hadiburpee@gmail.com>
2018-10-05 18:00:28 -0700

    initial html setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCaVgGul_tkr5MgRo_K1j2XhoNOZmbQa9w",
+    authDomain: "trainschedule-cf4ec.firebaseapp.com",
+    databaseURL: "https://trainschedule-cf4ec.firebaseio.com",
+    projectId: "trainschedule-cf4ec",
+    storageBucket: "",
+    messagingSenderId: "1086289403219"
+  };
+  firebase.initializeApp(config);
 
 
 
 
-
-
-
-
+  
 });//document ready bracket
