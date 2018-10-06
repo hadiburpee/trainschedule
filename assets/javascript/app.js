2009c7efb8679b147a42b17e53b8709829897fbb
hadiburpee <hadiburpee@gmail.com>
2018-10-05 22:24:29 -0700

    added input and display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-// Initialize Firebase
+//Initialize Firebase
   var config = {
     apiKey: "AIzaSyCaVgGul_tkr5MgRo_K1j2XhoNOZmbQa9w",
     authDomain: "trainschedule-cf4ec.firebaseapp.com",
@@ -11,8 +11,50 @@ $(document).ready(function(){
   };
   firebase.initializeApp(config);
 
-
-
-
   
+  var trainName = "";
+  var destination = "";
+  var firstTrain = "";
+  var frequencyMin = "";
+
+
+  $(".btn").click(function(){
+    trainName = $("#add-train-name").val().trim();
+    destination = $("#add-destination-name").val().trim();
+    firstTrain = $("#add-train-time").val().trim();
+    frequencyMin = $("#add-frequency-min").val().trim();
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(firstTrain);
+    console.log(frequencyMin);
+
+    addToDisplay(trainName, destination, firstTrain, frequencyMin);
+
+
+  });
+
+  function addToDisplay(tn, ds, ft, fm){
+    var p1 = $("<p>");
+    var p2 = $("<p>");
+    var p3 = $("<p>");
+    var p4 = $("<p>");
+
+    p1.text(tn);
+    p2.text(ds);
+    p3.text(fm);
+    p4.text(ft);
+
+    $("#train-name-display").append(p1);
+    $("#destination-display").append(p2);
+    $("#frequency-display").append(p3);
+    $("#next-arrival-display").append(p4);
+
+
+  }
+
+
+
+
+
 });//document ready bracket
